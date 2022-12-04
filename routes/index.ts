6867ed9ad568f78dd04f6e570fa608ce2e78df3b
ejaz4 <ejazali4@protonmail.ch>
2022-12-04 22:59:19 +0000

    ✨ CORS

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,13 @@
 import { apiRoutes } from "./api";
 import { staticRoutes } from "./static";
+import cors from '@fastify/cors'
+import { server } from "..";
 
 export const setRoutes = () => {
+    server.register(cors, {
+        origin: "*"
+    })
+
     apiRoutes();
     staticRoutes();
 }
